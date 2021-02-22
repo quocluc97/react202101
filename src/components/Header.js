fca811b3e1802d08c040c88774eb0fb43057cc8b
quocluc97 <quocluc97@gmail.com>
2021-02-22 22:10:21 +0700

    f

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+const Header = ({ title }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+Header.propTypes = { title: PropTypes.string.isRequired };
+// CSS in JS
+// const headingStyle={}
+
+
+export default Header;
